6222db49b80e3cc955abde332bfabab710f453c5
jigun058 <jigun058@gmail.com>
2025-10-15 20:37:33 +0900

    fix: @Controller, @GetMapping import 추가

--- app/src/main/java/dev/aloc/spring/testcomponents/HelloController.java
+++ app/src/main/java/dev/aloc/spring/testcomponents/HelloController.java
@@ -1,5 +1,8 @@
 package dev.aloc.spring.testcomponents;
 
+import dev.aloc.spring.annotation.Controller;
+import dev.aloc.spring.annotation.GetMapping;
+
 // 구현한 뒤 import 필요!!
 @Controller
 public class HelloController {
