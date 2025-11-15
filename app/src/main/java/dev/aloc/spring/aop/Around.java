591d801eab937a387b8974e0d45e4bb350c0a103
j2iiin <jxxniie05@gmail.com>
2025-11-15 18:23:31 +0900

    feat: AspectProxyHandler에 모든 클래스 공통 기능 (실행시간 측정) 구현

--- app/src/main/java/dev/aloc/spring/aop/Around.java
+++ app/src/main/java/dev/aloc/spring/aop/Around.java
@@ -8,4 +8,5 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Around {
+    String value(); // 포인트컷을 넣어 주면 이 어노테이션을 붙인 메서드 전후로 Aspect가 실행됨
 }
--- app/src/main/java/dev/aloc/spring/aop/AspectProxyHandler.java
+++ app/src/main/java/dev/aloc/spring/aop/AspectProxyHandler.java
@@ -5,8 +5,22 @@ import java.lang.reflect.Method;
 
 public class AspectProxyHandler implements InvocationHandler {
     
+    private final Class<?> target;
+    
+    public AspectProxyHandler(Class<?> target) {
+        this.target = target;
+    }
+    
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        return method.invoke(args);
+        // 실행시간 로그 찍기
+        long startTime = System.nanoTime();
+        Object result = method.invoke(target, args);
+        long endTime = System.nanoTime();
+        
+        System.out.println(
+            target.getName() + "." + method.getName() +
+                "실행 시간: " + (endTime - startTime) / 100000 + " ms");
+        return result;
     }
 }
