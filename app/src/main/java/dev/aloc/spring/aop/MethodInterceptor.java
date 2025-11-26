1f23adf34b9194b6bdbed22e2d017c465e52e261
2fill <ichaeu@naver.com>
2025-11-26 20:01:41 +0900

    docs: MethodInterceptor와 MethodInvocation에 설명 주석 추가

--- app/src/main/java/dev/aloc/spring/aop/MethodInterceptor.java
+++ app/src/main/java/dev/aloc/spring/aop/MethodInterceptor.java
@@ -2,7 +2,6 @@ package dev.aloc.spring.aop;
 
 public interface MethodInterceptor {
     
-    // 이 인터페이스의 구현체에서 실제 메소드 실행 전후의 기능을 추가함 (invoke() 메소드 안에 정의)
-    
+    // 이 인터페이스의 구현체에서 메소드 실행 전후 기능을 추가 (invoke() 메소드 안에 정의)
     Object invoke(MethodInvocation invocation) throws Throwable;
 }
--- app/src/main/java/dev/aloc/spring/aop/MethodInvocation.java
+++ app/src/main/java/dev/aloc/spring/aop/MethodInvocation.java
@@ -3,12 +3,12 @@ package dev.aloc.spring.aop;
 import java.lang.reflect.Method;
 
 public interface MethodInvocation {
-    
+    // 호출 대상 객체 정보
     Object getTarget();
-    
+    // 호출 대상 객체의 메서드 정보
     Method getMethod();
-    
+    // 호출 시 전달된 인자들
     Object[] getArgs();
-    
+    // 호출 대상 객체 메서드 실행
     Object proceed() throws Throwable;
 }
